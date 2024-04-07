eb5acf41ff872c59d156e99f0397daacec655402
nexus4812 <powerbatteryboy@gmail.com>
2024-04-07 15:23:03 +0900

    change Kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,9 +3,10 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+// use Symfony\Component\HttpKernel\Kernel as BaseKernel; // default
+use Bref\SymfonyBridge\BrefKernel;
 
-class Kernel extends BaseKernel
+class Kernel extends BrefKernel
 {
     use MicroKernelTrait;
 }
