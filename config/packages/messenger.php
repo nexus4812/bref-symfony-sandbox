5c3ae97b4b46f76c8fdc3d18597a0b87a2c46653
nexus4812 <powerbatteryboy@gmail.com>
2024-04-27 16:14:06 +0900

    change config

--- config/packages/messenger.php
+++ config/packages/messenger.php
@@ -5,10 +5,27 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    // When execute `symfony server:start` command
+    if ($containerConfigurator->env() === 'local') {
+        $containerConfigurator->extension('framework', [
+            'messenger' => [
+                'transports' => [
+                    'async_priority_normal'
+                ],
+            ],
+        ]);
+
+        return;
+    }
+
     $containerConfigurator->extension('framework', [
         'messenger' => [
-            'transports' => null,
-            'routing' => null,
+            'transports' => [
+                'async' => '%env(MESSENGER_TRANSPORT_DSN)%',
+            ],
+            'routing' => [
+                \App\Message\RequestCapture::class => 'async',
+            ],
         ],
     ]);
 };
